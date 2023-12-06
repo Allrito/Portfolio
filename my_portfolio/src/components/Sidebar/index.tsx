f799ce2eba7b6f68a293d0c213a164cecff41a09
Allrito <allan1.thamvongs@epitech.eu>
2023-12-06 17:44:05 +0100

    Fixing Logo animation fixing logo animation to draw the logo's SVG with GSAP modifying github link in sidebar

--- my_portfolio/src/components/Sidebar/index.tsx
+++ my_portfolio/src/components/Sidebar/index.tsx
@@ -33,7 +33,7 @@ const Sidebar = () => (
                 </a>
             </li>
             <li>
-                <a target='_blank' rel='noreferrer' href='https://github.com/AllanEpitech'>
+                <a target='_blank' rel='noreferrer' href='https://github.com/Allrito'>
                     <FontAwesomeIcon icon={faGithub} color='#4d4d4e' />
                 </a>
             </li>
